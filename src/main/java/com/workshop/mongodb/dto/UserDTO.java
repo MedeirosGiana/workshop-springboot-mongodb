e1794933e2261cadf86940d2e9ccb5bbaf4f1892
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-27 19:59:41 -0300

    Getting user by id

--- src/main/java/com/workshop/mongodb/dto/UserDTO.java
+++ src/main/java/com/workshop/mongodb/dto/UserDTO.java
@@ -1,8 +1,8 @@
 package com.workshop.mongodb.dto;
 
 import com.workshop.mongodb.domain.User;
-
 import java.io.Serializable;
+
 
 public class UserDTO implements Serializable {
     private static  final long serialVersionUID= 1L;
---
+++ src/main/java/com/workshop/mongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.workshop.mongodb.resources.exception;
+
+import com.workshop.mongodb.servicies.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError();
+        err.setTimeStamp(System.currentTimeMillis());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Não encontrado!");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
+
