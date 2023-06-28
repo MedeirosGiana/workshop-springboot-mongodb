5c0035efc2c4ac004c11584cd102b91cfbed7efd
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-27 21:05:32 -0300

    Insertion of user with post

--- src/main/java/com/workshop/mongodb/servicies/UserService.java
+++ src/main/java/com/workshop/mongodb/servicies/UserService.java
@@ -1,6 +1,7 @@
 package com.workshop.mongodb.servicies;
 
 import com.workshop.mongodb.domain.User;
+import com.workshop.mongodb.dto.UserDTO;
 import com.workshop.mongodb.repository.UserRepository;
 import com.workshop.mongodb.servicies.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,12 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return  repository.insert(obj);
+    }
+    public User fromDTO(UserDTO objDTO){
+        return  new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
     }
 }
