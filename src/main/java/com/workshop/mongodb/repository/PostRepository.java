064abf241aecc9dac4e223d19cd030ac4a066819
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-29 19:00:27 -0300

    Simple query with query methods

--- src/main/java/com/workshop/mongodb/repository/PostRepository.java
+++ src/main/java/com/workshop/mongodb/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.workshop.mongodb.repository;
 
 import com.workshop.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);//consulta de posts
 }
--- src/main/java/com/workshop/mongodb/resources/PostResource.java
+++ src/main/java/com/workshop/mongodb/resources/PostResource.java
@@ -1,13 +1,14 @@
 package com.workshop.mongodb.resources;
 
 import com.workshop.mongodb.domain.Post;
+import com.workshop.mongodb.resources.util.URL;
 import com.workshop.mongodb.servicies.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "posts")
@@ -20,4 +21,12 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearsh")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
