a73b085706ec3e0bf738ce124c31a76f638858fb
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-29 19:23:53 -0300

    Simple query with @query

--- src/main/java/com/workshop/mongodb/repository/PostRepository.java
+++ src/main/java/com/workshop/mongodb/repository/PostRepository.java
@@ -2,8 +2,12 @@ package com.workshop.mongodb.repository;
 
 import com.workshop.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContainingIgnoreCase(String text);//consulta de posts
+    @Query("{'title':{$regex:?0,$options:'i'}}")
+    List<Post> searchTitle(String text);//consulta de posts contendo um dado String no título
+    List<Post> findByTitleContainingIgnoreCase(String text);//consulta de posts com query methods
 }
--- src/main/java/com/workshop/mongodb/resources/PostResource.java
+++ src/main/java/com/workshop/mongodb/resources/PostResource.java
@@ -22,7 +22,7 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value = "/titlesearsh")
+    @GetMapping(value = "/titlesearch")
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
