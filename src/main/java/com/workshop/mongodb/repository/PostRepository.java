dd2d996822657718011646ade90bce1d5bd65901
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-29 22:42:28 -0300

    Query with various criteria

--- src/main/java/com/workshop/mongodb/repository/PostRepository.java
+++ src/main/java/com/workshop/mongodb/repository/PostRepository.java
@@ -4,10 +4,14 @@ import com.workshop.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
     @Query("{'title':{$regex:?0,$options:'i'}}")
     List<Post> searchTitle(String text);//consulta de posts contendo um dado String no título
     List<Post> findByTitleContainingIgnoreCase(String text);//consulta de posts com query methods
+
+    @Query("{$and:[{date:{$gte:?1}}, {date:{$lte:?2}}, {$or[{'title':{$regex:?0,$options:'i'}},{body:{$regex:?0}, $options:'i'}},{'comments.text':{$regex:?0, $options:'i'}}]}]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
