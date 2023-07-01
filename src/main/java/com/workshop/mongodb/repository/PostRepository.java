513e085368fae8bea3f1d72a036b4d71630b3f1b
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-30 22:18:37 -0300

    Refactoring query by PostRepository

--- src/main/java/com/workshop/mongodb/repository/PostRepository.java
+++ src/main/java/com/workshop/mongodb/repository/PostRepository.java
@@ -12,6 +12,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> searchTitle(String text);//consulta de posts contendo um dado String no título
     List<Post> findByTitleContainingIgnoreCase(String text);//consulta de posts com query methods
 
-    @Query("{$and:[{date:{$gte:?1}}, {date:{$lte:?2}}, {$or[{'title':{$regex:?0,$options:'i'}},{body:{$regex:?0}, $options:'i'}},{'comments.text':{$regex:?0, $options:'i'}}]}]}")
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
     List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
